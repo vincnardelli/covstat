558d137b043f4b299a591379e6ea700e518e8d28
Vincenzo Nardelli <vincnardelli@gmail.com>
2020-11-08 19:56:45 +0100

    dashboard

--- model/script.sh
+++
@@ -1,5 +0,0 @@
-# build docker
-docker build --tag covstat_model .
-
-# launch script
-docker run --rm --name my-running-script -v "$PWD":/usr/src/myapp -w /usr/src/myapp covstat_model python model.py
