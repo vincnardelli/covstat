1dfcfcdef9bce6388b450a8827a351a9e77d2fe8
vincnardelli <vincnardelli@users.noreply.github.com>
2021-01-13 17:36:01 +0000

    Update python script

--- vaccini/covstat_vaccini.py
+++ vaccini/covstat_vaccini.py
@@ -17,7 +17,9 @@ df['spread'] = round(df['spread'], 2)
 df.tail(1).to_csv("confronto_italia_germania_last.csv")
 
 df = vaccinations[vaccinations.location.isin(stati)]
-df.drop(columns=['iso_code', 'total_vaccinations', 'daily_vaccinations', 'daily_vaccinations_per_million'], inplace=True)
+df.drop(columns=['iso_code', 'total_vaccinations', 'daily_vaccinations', 'daily_vaccinations_per_million', 
+                 'people_fully_vaccinated', 'daily_vaccinations_raw', 'people_vaccinated', 'people_vaccinated_per_hundred', 
+                 'people_fully_vaccinated_per_hundred'], inplace=True)
 df = df.melt(id_vars = ['location', 'date'])
 df.reset_index(inplace=True)
 df.date = pd.to_datetime(df.date).dt.strftime('%d-%m-%Y')
@@ -26,7 +28,9 @@ df
 
 stati = ['Italy', 'Germany', 'United Kingdom', 'Spain', 'United States']
 df = vaccinations[vaccinations.location.isin(stati)]
-df.drop(columns=['iso_code'], inplace=True)
+df.drop(columns=['iso_code',
+                 'people_fully_vaccinated', 'daily_vaccinations_raw', 'people_vaccinated', 'people_vaccinated_per_hundred', 
+                 'people_fully_vaccinated_per_hundred'], inplace=True)
 df = df.melt(id_vars = ['location', 'date'])
 df = df.pivot(index = ['date', 'variable'], columns = 'location', values= 'value')
 df.reset_index(inplace=True)
@@ -101,3 +105,76 @@ confronto_regioni.data = pd.to_datetime(confronto_regioni.data).dt.strftime('%m-
 confronto_regioni.to_csv("confronto_regioni.csv")
 
 confronto_regioni
+
+dfIT = vaccinations[vaccinations['location']=='Italy'].iloc[2:,:]
+dati_ITALIA = np.array(dfIT.iloc[3:,3])/(10**6)
+x_ITALIA = np.arange(0,len(dati_ITALIA))/7
+
+# requisiti per scenario minimo e immunità di gregge
+
+over65 = 14*10**6
+operatori_sanitari = 6.5*10**5
+periodo = 30*9
+soglia_immunita = 70/100
+n_dosi = 2
+popolazione = 60*10**6
+obj_minimo = (over65+operatori_sanitari)*2
+obj_ideale = (70/100*60*10**6)*2
+days = np.arange(270,0,-1)[0:len(dfIT_soglie)]
+
+dfIT_soglie = dfIT[['date','total_vaccinations']]
+dfIT_soglie = dfIT_soglie.iloc[2:,:]
+dfIT_soglie.iloc[6,1] = 321077
+ll = len(dfIT_soglie)
+ 
+dfIT_soglie['minimo'] = minimo_daily*np.arange(1,ll+1)
+dfIT_soglie['immunità'] = immunità_daily*np.arange(1,ll+1)
+ 
+dfIT_soglie['daily'] = np.diff(np.append(14613,np.array(dfIT_soglie['total_vaccinations'])))
+
+dfIT_soglie['nane_min']=(obj_minimo-np.array(dfIT_soglie['total_vaccinations']))/days
+dfIT_soglie['nane_ideal']=(obj_ideale-np.array(dfIT_soglie['total_vaccinations']))/days
+
+
+minimo_daily = n_dosi*(over65+operatori_sanitari)/periodo
+immunità_daily = n_dosi*(soglia_immunita*popolazione)/periodo
+
+print(minimo_daily)
+print(immunità_daily)
+
+n_week = 3/4*52
+
+week_min = obj_minimo/n_week/10**6
+week_ideal = obj_ideale/n_week/10**6
+
+week_grid = np.arange(0,n_week+0.1)
+
+# piano vaccini
+week_vaccini = [0,13,26,39,52,65,78]
+
+piano_vaccini_diff = np.array([0,28.269*10**6,57.202*10**6,53.84*10**6,
+                 14.806*10**6,28.266*10**6,20.19*10**6])/10**6
+
+piano_vaccini = np.cumsum(piano_vaccini_diff)
+
+pianovaccini = pd.DataFrame({"week": week_vaccini, "Numero di dosi piano vaccini": piano_vaccini})
+pianovaccini
+
+vaccinazioni = pd.DataFrame({"week":x_ITALIA, "Vaccini effettuati": dati_ITALIA})
+vaccinazioni
+
+projection = pd.DataFrame({"week": week_grid, 
+                          "70% di vaccinati entro 30/09/21": week_grid*week_ideal, 
+                           "Over 65 e op.sanitari vaccinati entro 30/09/21": week_grid*week_min})
+projection
+
+projection = projection.merge(pianovaccini, how="left")
+projection = projection.merge(vaccinazioni, how="left")
+projection.to_csv("projection.csv")
+projection
+
+vaccinazioni = pd.DataFrame({"week":x_ITALIA, "Vaccini effettuati": dati_ITALIA, 
+                                                       "70% di vaccinati entro 30/09/21": x_ITALIA*week_ideal,
+                             "Over 65 e op.sanitari vaccinati entro 30/09/21": x_ITALIA*week_min 
+})
+vaccinazioni.to_csv("projection_vaccinazioni.csv")
